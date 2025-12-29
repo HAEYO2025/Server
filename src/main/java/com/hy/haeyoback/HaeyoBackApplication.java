0113afe7f9f210ab0a7679832e439ea88929db3b
randirao <randirao4245@gmail.com>
2025-12-29 17:18:09 +0900

    chore: 초기 세팅

---
+++ src/main/java/com/hy/haeyoback/HaeyoBackApplication.java
@@ -0,0 +1,13 @@
+package com.hy.haeyoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaeyoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaeyoBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hy/haeyoback/HaeyoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hy.haeyoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaeyoBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
