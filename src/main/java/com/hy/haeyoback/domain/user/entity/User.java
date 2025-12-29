e7c9feb0c32829e35f62d89a2a7be93db818f862
randirao <randirao4245@gmail.com>
2025-12-30 00:02:58 +0900

    chore: user.email 인덱스 추가

--- src/main/java/com/hy/haeyoback/domain/user/entity/User.java
+++ src/main/java/com/hy/haeyoback/domain/user/entity/User.java
@@ -5,10 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "users")
+@Table(
+        name = "users",
+        indexes = {
+                @Index(name = "idx_users_email", columnList = "email")
+        }
+)
 public class User {
 
     @Id
