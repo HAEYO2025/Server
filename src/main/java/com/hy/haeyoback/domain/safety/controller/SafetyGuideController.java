84b61061b2f620d0f9c746b7e273f0addfab1be4
randirao <randirao4245@gmail.com>
2025-12-30 13:46:31 +0900

    fix: 엔드포인트 수정

--- src/main/java/com/hy/haeyoback/domain/safety/controller/SafetyGuideController.java
+++ src/main/java/com/hy/haeyoback/domain/safety/controller/SafetyGuideController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/safety/guides")
+@RequestMapping("/api/safety/learning")
 public class SafetyGuideController {
 
     private final SafetyGuideService safetyGuideService;
